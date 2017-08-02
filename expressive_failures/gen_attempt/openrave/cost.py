61bb4d1fb55951aeece09c61957446ed6923f7ad
Minae Kwon <mk898@cornell.edu>
2017-08-02 15:41:16 -0700

    base can move in correct direction

--- expressive_failures/gen_attempt/openrave/cost.py
+++ expressive_failures/gen_attempt/openrave/cost.py
@@ -8,7 +8,7 @@ import scipy.spatial.distance
 
 """All cost functions"""
 
-def get_deltas(waypt, link_name,starting_config,goal_config):
+def get_deltas(waypt, link_names, starting_config, goal_config):
     """
     Computes deltaEE and deltaEL
 
@@ -16,29 +16,35 @@ def get_deltas(waypt, link_name,starting_config,goal_config):
     global robot, manip
     armids = list(manip.GetArmIndices()) #get arm indices
     links = robot.GetLinks()
-    link_idx = links.index(robot.GetLink(link_name))
+    deltaEEs=[]
+    deltaLs = []
+    for link_name in link_names:
+        link_idx = links.index(robot.GetLink(link_name))
 
-    #current
-    robot.SetDOFValues(waypt,armids) # set dof values to current config
-    EEcurr = manip.GetEndEffectorTransform()[:3][:,3] #get EE q
-    linkstrans = robot.GetLinkTransformations()
-    Lcurr = linkstrans[link_idx][:3][:,3] #get xyz of specific link
+        #current
+        robot.SetDOFValues(waypt,armids) # set dof values to current config
+        EEcurr = manip.GetEndEffectorTransform()[:3][:,3] #get EE q
+        linkstrans = robot.GetLinkTransformations()
+        Lcurr = linkstrans[link_idx][:3][:,3] #get xyz of specific link
 
-    #ideal
-    robot.SetDOFValues(goal_config,armids) #set dof valuue to ideal config
-    EEdesired = manip.GetEndEffectorTransform()[:3][:,3] #get EE qd    
-    linkstrans_d = robot.GetLinkTransformations()
-    Ldesired = linkstrans_d[link_idx][:3][:,3]
-    
-    #start
-    robot.SetDOFValues(starting_config,armids) #set dof valuue to starting config
-    linkstrans_s = robot.GetLinkTransformations()
-    Lstart = linkstrans_s[link_idx][:3][:,3]
-    EEstart = manip.GetEndEffectorTransform()[:3][:,3]
+        #ideal
+        robot.SetDOFValues(goal_config,armids) #set dof valuue to ideal config
+        EEdesired = manip.GetEndEffectorTransform()[:3][:,3] #get EE qd    
+        linkstrans_d = robot.GetLinkTransformations()
+        Ldesired = linkstrans_d[link_idx][:3][:,3]
+        
+        #start
+        robot.SetDOFValues(starting_config,armids) #set dof valuue to starting config
+        linkstrans_s = robot.GetLinkTransformations()
+        Lstart = linkstrans_s[link_idx][:3][:,3]
+        EEstart = manip.GetEndEffectorTransform()[:3][:,3]
 
-    deltaEE = (EEdesired-EEstart)
-    deltaL = (Lcurr-Lstart)
-    return deltaEE, deltaL
+        deltaEE = (EEdesired-EEstart)
+        deltaEEs.append(deltaEE)
+        deltaL = (Lcurr-Lstart)
+        deltaLs.append(deltaL)
+
+    return deltaEEs, deltaLs
 
 def get_rot_deltas(waypt, link_name, starting_config, goal_config):
     global robot, manip
@@ -123,7 +129,105 @@ def features_projections(waypt, starting_config, goal_config, d):
     The the second cost function we came up with that projects EL onto EE d times
     """
     link_name = 'r_elbow_flex_link'
-    deltaEE,deltaL = get_deltas(waypt, link_name, starting_config, goal_config)
+    link_names = ['r_elbow_flex_link', 'r_shoulder_lift_link']
+    deltaEEs,deltaLs = get_deltas(waypt, link_names, starting_config, goal_config)
+    norm_deltaEEs = []
+    norm_deltaLs = []
+    projections = []
+    for l in range(len(link_names)):
+        norm_deltaEE = np.linalg.norm(deltaEEs[l])
+        norm_deltaEEs.append(norm_deltaEE)
+        norm_deltaL = np.linalg.norm(deltaLs[l])
+        norm_deltaLs.append(norm_deltaL)
+
+    if (np.array(norm_deltaLs) < 1e-10).all():
+        proj = 0
+    else:
+        for l in range(len(link_names)):
+            init_projection = np.dot(deltaEEs[l],deltaLs[l])/norm_deltaEEs[l]
+            cos_theta = np.dot(deltaEEs[l],deltaLs[l])/(norm_deltaEEs[l] * norm_deltaLs[l])
+            proj = (init_projection*(cos_theta**(d-1)))
+            projections.append(proj)
+
+    return 0.1 - sum(projections)
+
+
+def base(waypt, starting_config, goal_config, d=3,coeff=1):
+    armids = list(manip.GetArmIndices()) #get arm indices
+    trans = robot.GetTransform()
+    curr_xy = waypt[-2:]
+    trans[:3][:,3][:2] = curr_xy
+    robot.SetTransform(trans)
+    links = robot.GetLinks()
+    #link_idx = links.index(robot.GetLink('base_laser_link'))
+    link_idx = links.index(robot.GetLink("torso_lift_motor_screw_link"))
+    #link_idx = links.index(robot.GetLink("r_shoulder_lift_link"))
+
+    linkstrans = robot.GetLinkTransformations()
+    Bcurr = linkstrans[link_idx][:3][:,3][:2] #get xy of specific link
+
+    #ideal
+    robot.SetDOFValues(goal_config,armids) #set dof value to ideal config
+    EEdesired = manip.GetEndEffectorTransform()[:3][:,3][:2] #get EE qd    
+ 
+    #start
+    robot.SetDOFValues(starting_config,armids) #set dof valuue to starting config
+    EEstart = manip.GetEndEffectorTransform()[:3][:,3][:2]
+
+    Bdes = (EEdesired-EEstart)
+
+    norm_ideal_base = np.linalg.norm(Bdes)
+    norm_curr_base = np.linalg.norm(Bcurr)
+    if norm_curr_base < 1e-10:
+        proj = 0
+    else:
+        init_projection = np.dot(Bdes, Bcurr)/norm_ideal_base
+        cos_theta = np.dot(Bdes, Bcurr)/(norm_curr_base*norm_ideal_base)
+        proj = (init_projection*(cos_theta**(d-1)))
+
+    print proj
+    print scalar 
+    scalar = .2-proj
+    return scalar*coeff #this works better than the projection 
+
+
+def get_delta(waypt, link_name,starting_config,goal_config):
+    """
+    Computes deltaEE and deltaEL
+    """
+    global robot, manip
+    armids = list(manip.GetArmIndices()) #get arm indices
+    links = robot.GetLinks()
+    link_idx = links.index(robot.GetLink(link_name))
+
+    #current
+    robot.SetDOFValues(waypt,armids) # set dof values to current config
+    EEcurr = manip.GetEndEffectorTransform()[:3][:,3] #get EE q
+    linkstrans = robot.GetLinkTransformations()
+    Lcurr = linkstrans[link_idx][:3][:,3] #get xyz of specific link
+
+    #ideal
+    robot.SetDOFValues(goal_config,armids) #set dof valuue to ideal config
+    EEdesired = manip.GetEndEffectorTransform()[:3][:,3] #get EE qd    
+    linkstrans_d = robot.GetLinkTransformations()
+    Ldesired = linkstrans_d[link_idx][:3][:,3]
+    
+    #start
+    robot.SetDOFValues(starting_config,armids) #set dof valuue to starting config
+    linkstrans_s = robot.GetLinkTransformations()
+    Lstart = linkstrans_s[link_idx][:3][:,3]
+    EEstart = manip.GetEndEffectorTransform()[:3][:,3]
+
+    deltaEE = (EEdesired-EEstart)
+    deltaL = (Lcurr-Lstart)
+    return deltaEE, deltaL
+
+def features_projection(waypt, starting_config, goal_config, d):
+    """
+    The the second cost function we came up with that projects EL onto EE d times
+    """
+    link_name = 'r_elbow_flex_link'
+    deltaEE,deltaL = get_delta(waypt, link_name, starting_config, goal_config)
     # deltaEE_x, deltaEE_y, deltaEE_z, deltaL_x, deltaL_y, deltaL_z = \
     #     get_rot_deltas(waypt, link_name, starting_config, goal_config)
 
@@ -155,9 +259,10 @@ def cost_projections(waypt, starting_config, goal_config, d=7, coeff=1):
     """
     The the second cost function we came up with that projects EL onto EE d times
     """
-    feature = features_projections(waypt, starting_config, goal_config, d)
-    return feature*coeff
+    feature = features_projections(waypt, starting_config, goal_config, d) #elbow+shoulder
+    #feature = features_projection(waypt, starting_config, goal_config, d) #elbow
 
+    return feature*coeff
 
 def feature_base(waypt):
     global base_positions,robot
@@ -165,7 +270,7 @@ def feature_base(waypt):
         transform = robot.GetTransform()#reset base to original position
         transform[:3][:,3] = base
         robot.SetTransform(transform) 
-        
+
 
 def cost_base(waypt, coeff=1):
     feature = features_base(waypt)
