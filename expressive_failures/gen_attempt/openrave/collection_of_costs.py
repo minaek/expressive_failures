dc40bb8c81fe62d1dfb5e320b29c91153da2036d
Sandy Huang <shhuang@cs.berkeley.edu>
2017-09-24 21:18:53 -0700

    Small tweaks to cost functions

--- expressive_failures/gen_attempt/openrave/collection_of_costs.py
+++ expressive_failures/gen_attempt/openrave/collection_of_costs.py
@@ -35,14 +35,14 @@ def features_projections(waypt, starting_config, goal_config, d):
         norm_deltaL = np.linalg.norm(deltaLs[l])
         norm_deltaLs.append(norm_deltaL)
 
-    if (np.array(norm_deltaLs) < 1e-10).all():
-        proj = 0
-    else:
-        for l in range(len(link_names)):
+    for l in range(len(link_names)):
+        if norm_deltaLs[l] < 1e-10:
+            proj = 0
+        else:
             init_projection = np.dot(deltaEEs[l],deltaLs[l])/norm_deltaEEs[l]
             cos_theta = np.dot(deltaEEs[l],deltaLs[l])/(norm_deltaEEs[l] * norm_deltaLs[l])
             proj = (init_projection*(cos_theta**(d-1)))
-            projections.append(proj)
+        projections.append(proj)
 
     return 0.1 - sum(projections)
 
@@ -89,21 +89,22 @@ def get_deltas(waypt, link_names, starting_config, goal_config):
 Cost Function 2
 """
 def l1_distance(waypt, starting_config, goal_config):
-	global robot, manip
+    global robot, manip
     armids = list(manip.GetArmIndices()) #get arm indices
-	link_name = 'r_elbow_flex_link'
-	link_idx = links.index(robot.GetLink(link_name))
+    link_name = 'r_elbow_flex_link'
+    links = robot.GetLinks()
+    link_idx = links.index(robot.GetLink(link_name))
 
-	#current EL coord
-	robot.SetDOFValues(waypt, armids)
-	linkstrans = robot.GetLinkTransformations()
-	ELq = linkstrans[link_idx][:3][:,3]
+    #current EL coord
+    robot.SetDOFValues(waypt, armids)
+    linkstrans = robot.GetLinkTransformations()
+    ELq = linkstrans[link_idx][:3][:,3]
 
-	#desired EL coord
-	robot.SetDOFValues(goal_config, armids)
-	linkstrans_d = robot.GetLinkTransformations()
-	ELd = linkstrans_d[link_idx][:3][:,3]
-	l1norm = np.linalg.norm((ELq-ELd), ord=1)
+    #desired EL coord
+    robot.SetDOFValues(goal_config, armids)
+    linkstrans_d = robot.GetLinkTransformations()
+    ELd = linkstrans_d[link_idx][:3][:,3]
+    l1norm = np.linalg.norm((ELq-ELd), ord=1)
 
     return 0.1 - l1norm
 
@@ -113,11 +114,12 @@ Cost Function 3
 def alignment(waypt, starting_config, goal_config):
 	global robot, manip
     armids = list(manip.GetArmIndices()) #get arm indices
-	link_name = 'r_shoulder_lift_link'
-	link_idx = links.index(robot.GetLink(link_name))
+    link_name = 'r_shoulder_lift_link'
+    links = robot.GetLinks()
+    link_idx = links.index(robot.GetLink(link_name))
 
-	#current EE, L
-	robot.SetDOFValues(waypt, armids)
+    #current EE, L
+    robot.SetDOFValues(waypt, armids)
     EEq = manip.GetEndEffectorTransform()[:3][:,3] #get EE q
     linkstrans = robot.GetLinkTransformations()
     Lq = linkstrans[link_idx][:3][:,3] #get xyz of specific link
